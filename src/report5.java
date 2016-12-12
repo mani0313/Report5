d96425de43c2b5b29e651f9925aed3b96877b98a
Aryu Shiroma <e165731@ie.u-ryukyu.ac.jp>
2016-12-12 22:50:41 +0900

    step2

--- src/report5.java
+++ src/report5.java
@@ -2,9 +2,15 @@
  * Created by e165731 on 2016/12/12.
  */
 public class report5 {
-    public static void main(String[]args) {
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[]args){
+        try{
+            String str=null;
+            System.out.println(str.length());
+        }
+        catch(NullPointerException s){
+            System.out.println("NullPointerExceptionが発生しました。");
+            s.printStackTrace();
+        }
     }
 }
 
