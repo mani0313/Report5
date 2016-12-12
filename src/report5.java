c31a5cd49dbb6bfe5dc6877d445370f88559d29f
Aryu Shiroma <e165731@ie.u-ryukyu.ac.jp>
2016-12-12 22:26:58 +0900

    ex1

---
+++ src/report5.java
@@ -0,0 +1,10 @@
+/**
+ * Created by e165731 on 2016/12/12.
+ */
+public class report5 {
+    public static void main(String[]args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
+
